c7c82190afa0d19f3f53b089b291a3871c4842ae
UnnatiBhalekar <unnatibhalekar12345@gmail.com>
2024-08-09 10:28:57 +0530

    Added CrossOrigin Annotation

--- src/main/java/com/example/demo/controllers/BagItemController.java
+++ src/main/java/com/example/demo/controllers/BagItemController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,12 +13,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.DTO.BagItemDTO;
-import com.example.demo.entity.BagItem;
+
 import com.example.demo.entity.BagWeightAndSpaceDTO;
 import com.example.demo.service.BagItemService;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "*") 
 public class BagItemController {
 
 	@Autowired
